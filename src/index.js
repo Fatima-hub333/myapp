a8ec00959614c4bfefa0d32ada0d8e0431d4de43
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-21 15:41:10 +0500

    use React State

--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ import './index.css'
 
 function App() {
   // eslint-disable-next-line no-unused-vars
-  const thingsArray = ["Thing 1", "Thing 2"]
-  const thingsElements = thingsArray.map(thing => <p key={thing}>{thing}</p>)
+  
+  
+  const [things, setThings] = React.useState(["Thing 1", "Thing 2"])
+
 
   function addItem() {
-    const newThingText = `New ${thingsArray.length + 1}`
-    thingsArray.push(newThingText)
-    document.getElementById()
-    console.log(thingsArray)
+    const newThingText = `New ${things.length + 1}`
+    setThings(prevState => [...prevState, newThingText])
   }
+  const thingsElements = things.map(thing => <p key={thing}>{thing}</p>)
   return (
     <div>
       <button onClick={addItem}>Add Item</button>
