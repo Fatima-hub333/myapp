47f482090d84a213c625ff3a575c5454f54d9219
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-27 14:54:15 +0500

    Box_challenge_3.1

--- src/Box.js
+++ src/Box.js
@@ -3,11 +3,23 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Box = props => {
+	const [on, setOn] = React.useState(props.on)
+	 /**
+     * 
+     * Create an event listener so when the box is clicked,
+     * it toggles from "on" to "off".
+     * 
+     * Goal: clicking each box should toggle it on and off.
+     */
 	const styles = {
-		backgroundColor: props.on ? "#222222" : "none"
+		backgroundColor: on ? "#222222" : "transparent"
+	}
+
+	function toggle() {
+		setOn(prevOn => !prevOn)
 	}
 	return (
-		<div style={styles}className="box"> 
+		<div style={styles}className="box" onClick={toggle}> 
     </div>
 	)
 }
