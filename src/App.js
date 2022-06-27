c1b1dd66fb5b53f841aea4ffc79fd08f7da40795
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-27 13:02:51 +0500

    Boxes challenge part-1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
+import boxes from './boxes'
 import './index.css'
 
-export default function App() {
-  
+export const App = () => {
+  const [squares, setSquares] = React.useState(boxes)
+  const squareElements = squares.map(square => (
+    <div className="box" key={square.id}></div>
+  ))
+  return (
+    <main>
+      {squareElements}
+    </main>
+  )
 }
---
+++ src/boxes.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const boxes = () => {
+  return (
+		[
+			{
+        id: 1,
+        on: true
+    },   
+    {
+        id: 2,
+        on: false
+    },   
+    {
+        id: 3,
+        on: true
+    },   
+    {
+        id: 4,
+        on: true
+    },   
+    {
+        id: 5,
+        on: false
+    },   
+    {
+        id: 6,
+        on: false
+    },
+      
+      
+      ]
+  )
+}
+
+export default boxes
