8e65c883a6b62090021c52bdcb48e9932a49c17b
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-27 12:05:05 +0500

    practice useState

--- src/App.js
+++ src/App.js
@@ -2,5 +2,39 @@ import React from 'react'
 import './index.css'
 
 export default function App() {
-  
+  const [contact, setContact] = React.useState({
+    firstName: "Fatima",
+    lastName: "Zahir",
+    phone: "+92 347 7871262",
+    email: "fatimaazahir@gmail.com",
+    isFavorite: false
+  })
+
+  let starIcon = contact.isFavorite ? "star-filled.png" : "star-empty.png"
+
+  function toggleFavorite() {
+    setContact(prevContact => ({
+      ...prevContact,
+      isFavorite: !prevContact.isFavorite
+    }))
+  }
+
+  return (
+    <main>
+      <article className="card">
+        <img src="../images/user.png" className="card--image" />
+        <div className="card--info">
+          <img src={`../images/${starIcon}`}
+            className="card--favorite"
+            onClick={toggleFavorite}
+          />
+          <h2 className="card--name">
+            {contact.firstName} {contact.emaillastName}
+          </h2>
+          <p className="card--contact">{contact.phone}</p>
+          <p className="card--contact">{contact.email}</p>
+        </div>
+      </article>
+    </main>
+  )
 }
--- src/index.js
+++ src/index.js
@@ -1,25 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './App'
 import './index.css'
 
-function App() {
-  // eslint-disable-next-line no-unused-vars
-  
-  
-  const [things, setThings] = React.useState(["Thing 1", "Thing 2"])
-
-
-  function addItem() {
-    const newThingText = `New ${things.length + 1}`
-    setThings(prevState => [...prevState, newThingText])
-  }
-  const thingsElements = things.map(thing => <p key={thing}>{thing}</p>)
-  return (
-    <div>
-      <button onClick={addItem}>Add Item</button>
-      {thingsElements}
-    </div>
-  )
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
