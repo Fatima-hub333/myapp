ba228d6839866a0425c028d31a21a08abe35ee43
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-29 14:54:39 +0500

    practice ternary conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 import React from "react"
-import Joke from "./Joke"
-import jokesData from "./jokesData"
 
 export default function App() {
-    const jokeElements = jokesData.map(joke => {
-        return (
-            <Joke 
-                key={joke.id}
-                setup={joke.setup} 
-                punchline={joke.punchline} 
-            />
-        )
-    })
-    return (
-        <div>
-            {jokeElements}
-        </div>
-    )
+  const [messages, setMessages] = React.useState(["a", "b"])
+  
+  return (
+    <div>
+      {
+        messages.length === 0 ?
+        <h1>You're all caught up!</h1> :
+        <h1>You have {messages.length} unread  
+        {messages.length > 1 ? "messages" : "message"}
+        </h1>
+      }
+    </div>
+  )
 }
