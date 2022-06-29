2626fda5caf0cc98ec129e4659fd864d7f1e6cb9
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-29 11:28:37 +0500

    Box challenge part-4

--- src/App.js
+++ src/App.js
@@ -5,24 +5,40 @@ import Box from "./Box"
 import './index.css'
 
 export default function App() {
-    const [squares, setSquares] = React.useState(boxes)
-
-    function toggle(id) {
-    console.log(id)
-}
-    
-    const squareElements = squares.map(square => (
-        <Box 
-        key={square.id} 
-        id={square.id}
-        on={square.on} 
-        toggle={toggle}
-        />
-    ))
-    
-    return (
-        <main>
-            {squareElements}
-        </main>
-    )
+  const [squares, setSquares] = React.useState(boxes)
+  
+  function toggle(id) {
+    setSquares(prevSquares => {
+      const newSquares = []
+      for (let i = 0; i < prevSquares.length; i++) {
+        const currentSquare = prevSquares[i]
+        if (currentSquare.id === id) {
+          const updateSquare = {
+            ...currentSquare,
+            on: !currentSquare.on
+          }
+          newSquares.push(updateSquare)
+        } else {
+          newSquares.push(currentSquare)
+        }
+      }
+      return newSquares
+    })
+  }
+  
+  
+  const squareElements = squares.map(square => (
+      <Box 
+      key={square.id} 
+      id={square.id}
+      on={square.on} 
+      toggle={toggle}
+      />
+  ))
+  
+  return (
+      <main>
+          {squareElements}
+      </main>
+  )
 }
