7b0ec83eaf015cac7eeb451db954512ba0b30bdc
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-30 22:26:49 +0500

    Form state object practice

--- src/Form.js
+++ src/Form.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Form = () => {
   const [formData, setFormData] = React.useState(
-    { firstName: "", lastName: "" }
+    { firstName: "", lastName: "", email: ""}
   )
 console.log(formData)
   function handleChange(event) {
@@ -28,6 +28,12 @@ console.log(formData)
         placeholder='Last Name'
         onChange={handleChange}
         name="lastName"
+      />
+      <input
+        type="email"
+        placeholder="email"
+        onChange={handleChange}
+        name="email"
       />
     </form>
   )
