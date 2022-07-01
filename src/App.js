aa5209d3321390e73887a506cbd6cb96b587aaf1
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-07-01 19:52:08 +0500

    Create Signup-form & give it styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
 import React from 'react'
 import './index.css'
 
-export default function App() {
-  
+const App = () => {
+  return (
+    <div className='form--container'>
+      <form className='form'>
+        <input
+          type="email"
+          placeholder='Email Address'
+          className='form--input'
+        />
+        <input
+          type="password"
+          placeholder='Password'
+          className='form--input'
+        />
+        <input
+          type="password"
+          placeholder='Confirm Password'
+          className='form--input'
+        />
+
+        <div className='form--marketing'>
+          <input
+            type="checkbox"
+            id='okayToEmail'
+          />
+          <label htmlFor='okayToEmail'> I want to join the newsletter</label>
+        </div>
+        <button className='form--submit'>Sign Up</button>
+      </form>
+    </div>
+  )
 }
+
+export default App
