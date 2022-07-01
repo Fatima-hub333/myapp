74807d6ea686cfaf063f54738c0e2dd5147f991c
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-07-01 16:06:27 +0500

    Forms: submitting the form

--- src/Form.js
+++ src/Form.js
@@ -26,8 +26,14 @@ const Form = () => {
     })
   }
 
+  function handleSubmit(event) {
+    event.preventDefault()
+    // submitToApi(formData)
+    console.log(formData)
+  }
+
   return (
-      <form>
+      <form onSubmit={handleSubmit}>
       <input 
         type="text"
         placeholder="First Name"
@@ -121,6 +127,10 @@ const Form = () => {
         <option value='indigo'>Indigo</option>
         <option value='violet'>Violet</option>
       </select>
+      <br />
+      <br />
+
+      <button>Submit</button>
     </form>
   )
 }
