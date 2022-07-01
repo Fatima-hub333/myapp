ba87589c88ad111d7650cab226840503c986a53b
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-07-01 20:44:15 +0500

    create signup-form

--- src/App.js
+++ src/App.js
@@ -2,29 +2,69 @@ import React from 'react'
 import './index.css'
 
 const App = () => {
+  const [formData, setFormData] = React.useState({
+    email: "",
+    password: "",
+    passwordConfirm: "",
+    joinedNewsletter: true
+  })
+
+  function handleChange(event) {
+    const { name, value, type, checked } = event.target
+    setFormData(prevFormData => ({
+      ...prevFormData,
+      [name]: type === "checkbox" ? checked : value
+    }))
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    if (formData.password === formData.passwordConfirm) {
+      console.log("Successfully signed up")
+    } else {
+      console.log("Password do not match")
+      return
+    }
+
+    if (formData.joinedNewsletter) {
+      console.log("Thanks for signing up for our newsletter!")
+    }
+  }
   return (
     <div className='form--container'>
-      <form className='form'>
+      <form className='form' onSubmit={handleSubmit}>
         <input
           type="email"
           placeholder='Email Address'
           className='form--input'
+          name="email"
+          onChange={handleChange}
+          value={formData.email}
         />
         <input
           type="password"
           placeholder='Password'
           className='form--input'
+          name="password"
+          onChange={handleChange}
+          value={formData.password}
         />
         <input
           type="password"
           placeholder='Confirm Password'
           className='form--input'
+          name="passwordConfirm"
+          onChange={handleChange}
+          value={formData.passwordConfirm}
         />
 
         <div className='form--marketing'>
           <input
             type="checkbox"
             id='okayToEmail'
+            name="joinedNewsletter"
+            onChange={handleChange}
+            checked={formData.joinedNewsletter}
           />
           <label htmlFor='okayToEmail'> I want to join the newsletter</label>
         </div>
