29a3d59b540133e3f84acde460b3d94987fb100c
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-07-04 22:50:16 +0500

    useEffect dependencies

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import './index.css'
 
 const App = () => {
-    const [starWarsData, setStarWarsData] = React.useState({})
+  const [starWarsData, setStarWarsData] = React.useState({})
+  const [count, setCount] = React.useState(0)
 
-    React.useEffect(function () {
-      fetch("https://swapi.dev/api/people/1")
-       .then(res => res.json())
-       .then(data => setStarWarsData(data))
-    })
+  console.log("Component Render")
+  React.useEffect(function () {
+      console.log("Effect ran")
+      // fetch("https://swapi.dev/api/people/1")
+      //  .then(res => res.json())
+      //  .then(data => setStarWarsData(data))
+    }, [0])
   
   return (
       <div>
-          <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+      <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+      <h2>The count is {count}</h2>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
       </div>
   )
 }
