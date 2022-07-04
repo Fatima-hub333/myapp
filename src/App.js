9d5daf07d875637c3d6d365dde0fa3d21e27f732
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-07-04 22:31:07 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import './index.css'
 
-export default function App() {
-  
+const App = () => {
+    const [starWarsData, setStarWarsData] = React.useState({})
+  return (
+      <div>
+          <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+      </div>
+  )
 }
+
+export default App
