05092dde2d8dc2c115c7b3b55f13ea8991fdc2dd
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-07-04 22:39:06 +0500

    useEffect() syntax and default behavior

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import './index.css'
 
 const App = () => {
     const [starWarsData, setStarWarsData] = React.useState({})
+
+    React.useEffect(function () {
+      fetch("https://swapi.dev/api/people/1")
+       .then(res => res.json())
+       .then(data => setStarWarsData(data))
+    })
+  
   return (
       <div>
           <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
