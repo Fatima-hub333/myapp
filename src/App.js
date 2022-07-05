3d7343b5ef15007322c3b9edf7090b24e08b0aea
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-07-05 12:51:08 +0500

    Create a layout of the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Main from './components/Main'
 import './index.css'
 
-export default function App() {
-  
+const App = () => {
+  return (
+    <div className="container">
+      <Navbar />
+      <Main />
+    </div>
+  )
 }
+
+export default App
